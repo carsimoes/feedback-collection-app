415ef874e425c90d52829fded4227ed3c9917558
tonare <caarlos.renato.simoes@gmail.com>
2019-09-05 21:03:16 -0300

    new node server lines- heroku

--- server/index.js
+++ server/index.js
@@ -5,4 +5,5 @@ app.get('/', (req,res) => {
     res.send({ hi: 'there'});
 });
 
-app.listen(5000); //node index.js to run!
+const PORT = process.env.PORT || 5000;
+app.listen(PORT); //node index.js to run!
