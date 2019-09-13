677267237bc8d6ee3767fda4335f6ed19e55c9b2
tonare <caarlos.renato.simoes@gmail.com>
2019-09-12 21:37:48 -0300

    Authentication with Google OAuth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send({ hi: 'there'});
-});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken) => {
+            console.log(accessToken);
+        })
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope:['profile','email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT); //node index.js to run!
+app.listen(PORT); 
+
+
+
+
+// app.get('/', (req,res) => {
+//     res.send({ bye: 'buddy'});
+// });
+//node index.js to run!!
+
+//Redeploy
+// git add, git commit, git push heroku master
