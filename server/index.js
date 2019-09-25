8f70d5f85c8a7f84bc775d30891d7161af305759
tonare <carlos.renato.simoes@gmail.com>
2019-09-25 09:08:49 -0300

    first model class: user

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
 require('./services/passport');
+require('./models/user');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT); 
+app.listen(PORT);
 
 //npm run dev
 
