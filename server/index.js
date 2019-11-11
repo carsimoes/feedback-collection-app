b2a68399604580c1e534864db71b0c6f35a62cfa
tonare <caarlos.renato.simoes@gmail.com>
2019-11-11 20:30:55 -0300

    testing authentication

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys.js'); //https://console.developers.google.com
 require('./models/user');
 require('./services/passport'); //order matters
@@ -7,6 +9,16 @@ require('./services/passport'); //order matters
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser(
+    (user,done) => {
+        done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id).then(user=>{
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -21,10 +32,12 @@ passport.use(
                     .then((existingUser)=>{
                         if(existingUser){
                             //we already have a user
+                            done(null, existingUser);
                         }else{
-                            new User({googleId: profile.id}).save();
+                            new User({googleId: profile.id})
+                                .save().then(user=>done(null, user));
                         }
-                    })
+                    });
             
         })
 );
